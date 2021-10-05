00e820514960672a363938c88c33160bce221c66
Kazuru <esinnik0311@gmail.com>
2021-10-05 19:16:23 +0300

    # Titles v1.0 ## Add initial models

--- api_yamdb/Yamdb/admin.py
+++ api_yamdb/Yamdb/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Category, Genre, Title
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+#@admin.register(User)
+#class UserAdmin(admin.ModelAdmin):
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'rating', 'description', 'get_genres',
+                    'category')
+    search_fields = ('name', 'description')
+    list_filter = ('year', 'rating', 'genre', 'category')
--- api_yamdb/Yamdb/models.py
+++ api_yamdb/Yamdb/models.py
@@ -22,3 +22,54 @@ class User(AbstractUser):
         choices=UserRole.choices,
         default=UserRole.USER,
     )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название категории')
+    slug = models.SlugField(max_length=150, unique=True,
+                            verbose_name='Уникальный идентификатор категории')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name[:20]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Жанр')
+    slug = models.SlugField(max_length=150, unique=True,
+                            verbose_name='Уникальный идентификатор жанра')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name[:20]
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    year = models.PositiveSmallIntegerField()
+    rating = models.FloatField()
+    description = models.TextField(verbose_name='Описание произведения')
+    genre = models.ManyToManyField(Genre, related_name='genre_titles',
+                                   verbose_name='Жанр')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='category_titles',
+                                 verbose_name='Категория')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def get_genres(self):
+        return ",".join([str(gen) for gen in self.genre.all()])
+
+    def __str__(self):
+        return self.name[:20]
